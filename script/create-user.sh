de6c2e4dcada95bdfb446a6724b170bc75d593c1
heiwa4126 <heiwa4126@gmail.com>
2026-08-05 15:32:58 +0900

    [WIP] scripts writing...

--- script/create-user.sh
+++ script/create-user.sh
@@ -7,5 +7,5 @@ sudo useradd --system --home-dir /dev/null --shell /usr/sbin/nologin "$username"
 
 # my-app 用バイナリ置き場を作る
 sudo mkdir -p "$user_dir"
-sudo chown -R "$username":"$gid" "$user_dir"
-sudo chmod -R 750 "$user_dir"
+sudo chown "$username":"$gid" "$user_dir"
+sudo chmod 770 "$user_dir"
