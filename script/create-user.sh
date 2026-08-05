7aa66cb46f88d2300191fa7247d6bed99218da56
heiwa4126 <heiwa4126@gmail.com>
2026-08-05 15:14:26 +0900

    [WIP] scripts writing...

--- script/create-user.sh
+++ script/create-user.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # 実行専用ユーザを作る(ログイン不可)
 sudo useradd --system --home-dir /dev/null --shell /usr/sbin/nologin "$username"
 
-# バイナリ置き場を作る
-sudo mkdir -p "$server_dir"
+# my-app 用バイナリ置き場を作る
+sudo mkdir -p "$user_dir"
 sudo chown -R "$username":"$gid" "$user_dir"
 sudo chmod -R 750 "$user_dir"
